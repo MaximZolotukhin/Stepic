4a6f90cafbd6df34f9685173c5bf57407fa8c86a
Maxim <ScorpionEvil777@gmail.com>
2021-12-14 22:26:36 +0300

    lesson_3.4 end

---
+++ Lesson_3/lesson_3.4_part_4.py
@@ -0,0 +1,12 @@
+"""
+3.4 Спецсимволы и экранирование символов
+
+Подвиг 5. Используя raw-строки, задайте строку, содержащую этот путь к файлу:
+C:\WINDOWS\System32\drivers\etc\hosts. Результат отобразите на экране.
+
+Sample Input:
+Sample Output: C:\WINDOWS\System32\drivers\etc\hosts
+"""
+
+# префикс r выводит строку в том виде в котором ее передают.
+print(r'C:\WINDOWS\System32\drivers\etc\hosts')
